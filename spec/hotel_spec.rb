68ee346ed513e157d041e5b68d0b21963b661e58
patriciapaula <patricia.paula@gmail.com>
2023-05-15 19:49:24 -0300

    sending first test

--- spec/hotel_spec.rb
+++ spec/hotel_spec.rb
@@ -1,11 +1,17 @@
-# frozen_string_literal: true
-
 RSpec.describe Hotel do
-  it "has a version number" do
-    expect(Hotel::VERSION).not_to be nil
+
+  describe '.search' do
+    context 'when given a valid hotel name' do
+      let(:name) { 'DoubleTree Hilton Amsterdam' }
+
+      it 'returns the URLs of the hotel from different sources' do
+        urls = Hotel.search(name)
+
+        expect(urls[0]).to start_with('https://www.tripadvisor.com/')
+        expect(urls[1]).to start_with('https://www.booking.com/')
+        expect(urls[2]).to start_with('https://www.holidaycheck.de/')
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
